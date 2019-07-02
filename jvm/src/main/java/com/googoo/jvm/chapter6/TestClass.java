341bec1191b470e12c028e2f8dfa92cdda686841
teng.sun <teng.sun@rograndec.com>
2019-07-02 10:33:27 +0800

    jvm 字节码学习

---
+++ jvm/src/main/java/com/googoo/jvm/chapter6/TestClass.java
@@ -0,0 +1,20 @@
+package com.googoo.jvm.chapter6;
+
+/**
+ * @author sunteng
+ * @create 2019-07-01 上午 8:14
+ **/
+public class TestClass {
+
+    private int m;
+
+    public int inc(){
+        return m + 1;
+    }
+
+
+    public static void main(String[] args) {
+        Integer k = 222222;
+        System.out.println("222222".equals(k.toString()));
+    }
+}
