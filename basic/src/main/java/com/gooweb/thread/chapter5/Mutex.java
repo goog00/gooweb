32a5150796dd0ff93e6763b25240c4b115befa18
teng.sun <teng.sun@rograndec.com>
2019-07-24 09:19:50 +0800

    基于aqs实现自定义同步组件Mutex

--- basic/src/main/java/com/gooweb/thread/chapter5/Mutex.java
+++ basic/src/main/java/com/gooweb/thread/chapter5/Mutex.java
@@ -11,12 +11,14 @@ import java.util.concurrent.locks.Lock;
  **/
 public class Mutex implements Lock {
 
+    //静态内部类，自定义同步器
     private static class Sync extends AbstractQueuedSynchronizer{
         //是否处于占用状态
         protected boolean isHeldExclusively(){
             return getState() == 1;
         }
 
+        //当状态为0的时候获取锁
         public boolean tryAcquire(int acquires){
             if(compareAndSetState(0,1)){
                 setExclusiveOwnerThread(Thread.currentThread());
@@ -25,42 +27,52 @@ public class Mutex implements Lock {
             return false;
         }
 
+        //释放锁，将状态设置为0
+        protected boolean tryRelease(int releases){
+            if(getState() == 0){
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+        //返回一个condition，每个condition都包含了一个condition队列
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+
     }
 
 
-
-
-
-
-
-
+    //仅需要经操作代理到Sync上即可
+    private final Sync sync = new Sync();
     @Override
     public void lock() {
-
+        sync.acquire(1);
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-
+        sync.acquireInterruptibly(1);
     }
 
     @Override
     public boolean tryLock() {
-        return false;
+        return sync.tryAcquire(1);
     }
 
     @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1,unit.toNanos(timeout));
     }
 
     @Override
     public void unlock() {
-
+        sync.release(1);
     }
 
     @Override
     public Condition newCondition() {
-        return null;
+        return sync.newCondition();
     }
 }
