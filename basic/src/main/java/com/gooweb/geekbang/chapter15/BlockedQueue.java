76c731c9f04e00b6389803f9c754ec90b0139c87
teng.sun <teng.sun@rograndec.com>
2019-07-26 09:07:35 +0800

    读写锁

---
+++ basic/src/main/java/com/gooweb/geekbang/chapter15/BlockedQueue.java
@@ -0,0 +1,58 @@
+package com.gooweb.geekbang.chapter15;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 阻塞队列
+ *
+ * @author sunteng
+ * @create 2019-07-26 上午 8:32
+ **/
+public class BlockedQueue<T> {
+
+    final Lock lock = new ReentrantLock();
+
+    //条件变量：队列不满
+    final Condition notFull = lock.newCondition();
+
+    //条件变量：队列为空
+    final Condition notEmpty = lock.newCondition();
+
+
+//    //入队
+//    void enq(T x){
+//        lock.lock();
+//        try{
+//            while (队列已满){
+//                //等待队列不满
+//                notFull.await();
+//            }
+//
+//            // 入队后，通知可出队
+//            notEmpty.signal();
+//        } finally {
+//            lock.unlock();
+//        }
+//
+//    }
+//
+//    //出队
+//    void deq(){
+//        lock.lock();
+//
+//        try{
+//            while ("队列已空"){
+//                //等待队列不空
+//                notEmpty.await();
+//            }
+//
+//            notFull.signal();
+//        } finally {
+//            lock.unlock();
+//        }
+//
+//    }
+
+}
---
+++ basic/src/main/java/com/gooweb/geekbang/chapter17/Cache.java
@@ -0,0 +1,47 @@
+package com.gooweb.geekbang.chapter17;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 使用读写锁实现缓存
+ * @author sunteng
+ * @create 2019-07-26 上午 8:59
+ **/
+public class Cache<K,V> {
+    final Map<K,V> m = new HashMap<>();
+
+    final ReadWriteLock rwl = new ReentrantReadWriteLock();
+
+    //读锁
+    final Lock r = rwl.readLock();
+
+    //写锁
+    final Lock w = rwl.writeLock();
+
+    //读缓存
+    V get(K key){
+        r.lock();
+        try{
+            return m.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+
+    //写缓存
+
+    V put(K key, V v){
+        w.lock();
+        try{
+            return m.put(key,v);
+        } finally {
+            w.unlock();
+        }
+    }
+}
