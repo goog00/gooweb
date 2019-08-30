c894a153b0e971b2817b98c1802d6cea6ea868e2
teng.sun <teng.sun@rograndec.com>
2019-08-30 08:40:19 +0800

    3.无重复字符子串长度

--- basic/src/main/java/com/gooweb/leetcode/medium/Solution_3.java
+++ basic/src/main/java/com/gooweb/leetcode/medium/Solution_3.java
@@ -10,23 +10,38 @@ import java.util.Map;
  **/
 public class Solution_3 {
 
+    /**
+     * 主要思路是滑动窗口
+     * @param s
+     * @return
+     */
     public int lengthOfLongestSubstring(String s){
-        int n = s.length(),ans = 0;
+        int n = s.length();
+        int ans = 0; //子串长度
+        int left = 0;//记录子串的最左边字符的索引位置
+
+        //key:字符值;value :字符值的索引+1;
         Map<Character,Integer> map = new HashMap<>();//当前索引的字符串值
 
-        for(int j = 0, i = 0; j < n;j++){
+        for(int j = 0; j < n;j++){
             if(map.containsKey(s.charAt(j))){
-                i = Math.max(map.get(s.charAt(j)),i);
+                //有重复的字符,则取当前重复字符的位置索引与left中的最大值
+                left = Math.max(map.get(s.charAt(j)),left);
+                //本来以为可以使用下面的代码来获取最左边字符位置;但是忽略了和其他字符子串长度进行比较;
+//                left = map.get(s.charAt(j));
+
             }
-            ans = Math.max(ans,j - i + 1);//(j - i + 1) 表示子串的长度
-            map.put(s.charAt(j), j+ 1);
+            //(j - i + 1) 表示无重复子串的长度;每次保留最大的子串长度
+            ans = Math.max(ans,j - left + 1);
+
+            map.put(s.charAt(j), j + 1);
         }
         return ans;
     }
 
     public static void main(String[] args) {
         Solution_3 solution_3 = new Solution_3();
-        String kk = "abcabcbb";
+        String kk = "abcabcbab";
         int i = solution_3.lengthOfLongestSubstring(kk);
         System.out.println(i);
     }
