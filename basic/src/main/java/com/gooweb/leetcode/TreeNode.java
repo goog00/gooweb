c331f8a474d47149df099a6926c45b8893a187c6
teng.sun <teng.sun@rograndec.com>
2019-09-19 16:59:21 +0800

    重写equals hashcode 方法

--- basic/src/main/java/com/gooweb/leetcode/TreeNode.java
+++ basic/src/main/java/com/gooweb/leetcode/TreeNode.java
@@ -1,5 +1,7 @@
 package com.gooweb.leetcode;
 
+import java.util.Objects;
+
 /**
  * @author sunteng
  * @create 2019-08-23 上午 7:55
@@ -10,5 +12,20 @@ public class TreeNode {
     public TreeNode right;
     public TreeNode(int x){
         val = x;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
     }
 }
